fd05e44242300e6d355c0d09fba67ced510de9f5
Fabianhu <fabianhu@users.noreply.github.com>
2010-02-02 01:10:39 +0000

    xmega RETI issue adc init

--- AVR/adc.h
+++ AVR/adc.h
@@ -1,3 +1,18 @@
+
+/*! Offset addresses for production signature row on which is not in current GCC
+ *  header files
+ * \name GCC compatibility macros
+ */
+//@{
+#ifndef ADCACAL0_offset
+
+#define ADCACAL0_offset 0x20
+#define ADCACAL1_offset 0x21
+#define ADCBCAL0_offset 0x24
+#define ADCBCAL1_offset 0x25
+#endif
+//@}
+
 void ADCinit(void);
 
 void ADCStartConvCh(uint8_t c);
