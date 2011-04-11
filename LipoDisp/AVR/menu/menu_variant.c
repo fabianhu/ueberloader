5919ff87be444eee43da12c364da10d8c3af3df9
admin <admin@5e083c7a-a129-45ab-8a5b-46b92a8de37d>
2011-04-11 21:07:34 +0000

    Frequenz einstellbar

--- LipoDisp/AVR/menu/menu_variant.c
+++ LipoDisp/AVR/menu/menu_variant.c
@@ -41,134 +41,86 @@ void entermenu(void);
 
 //******** START OF AUTO-GENERATED CODE DO NOT EDIT!!! *********
 // Text definitions
-char txtMAINMENU[] PROGMEM="Main Menu";
-char txtCURRENTSETPOINT[] PROGMEM="Current Setpoint";
-char txtACTION[] PROGMEM="Action";
-char txtBATTTYPESELECT[] PROGMEM="Batt Type Select";
-char txtSETUP[] PROGMEM="Setup";
-char txtBACK[] PROGMEM="(back)";
-char txtCHARGEFULL[] PROGMEM="Charge Full";
-char txtCHARGESTORAGE[] PROGMEM="Charge Storage";
-char txtDISCHARGE[] PROGMEM="Discharge";
-char txtLIPO[] PROGMEM="LiPo";
-char txtLIFEPO[] PROGMEM="LiFePo4";
-char txtLIION[] PROGMEM="LiIon";
-char txtCELLCOUNT[] PROGMEM="CellCount";
-char txtBATTSETTINGS[] PROGMEM="Batt Settings";
-char txtCHARGELIMITS[] PROGMEM="Charge Limits";
-char txtPWM[] PROGMEM="Pwm";
-char txtBALACTIVVOLTAGE[] PROGMEM="Bal. Activ. Voltage";
-char txtCHARGEVOLTAGE[] PROGMEM="Charge Voltage";
-char txtSTORAGEVOLTAGE[] PROGMEM="Storage Voltage";
-char txtDISCHARGEVOLTAGE[] PROGMEM="Discharge Voltage";
-char txtLIFEPOVOLT[] PROGMEM="LiFePo4 Volt.";
-char txtLIIONVOLTAGE[] PROGMEM="LiIon Voltage";
-char txtCAPACITY[] PROGMEM="Capacity";
-char txtTIME[] PROGMEM="Time";
-char txtFEQUENCY[] PROGMEM="Fequency";
-char txtREFRESHPERIOD[] PROGMEM="Refresh-period";
+char	txtMAINMENU[] 	PROGMEM="Main Menu";
+char	txtCURRENTSETPOINT[] 	PROGMEM="Current Setpoint";
+char	txtACTION[] 	PROGMEM="Action";
+char	txtBATTTYPESELECT[] 	PROGMEM="Batt Type Select";
+char	txtSETUP[] 	PROGMEM="Setup";
+char	txtBACK[] 	PROGMEM="(back)";
+char	txtCHARGEFULL[] 	PROGMEM="Charge Full";
+char	txtCHARGESTORAGE[] 	PROGMEM="Charge Storage";
+char	txtDISCHARGE[] 	PROGMEM="Discharge";
+char	txtLIPO[] 	PROGMEM="LiPo";
+char	txtLIFEPO[] 	PROGMEM="LiFePo4";
+char	txtLIION[] 	PROGMEM="LiIon";
+char	txtCELLCOUNT[] 	PROGMEM="CellCount";
+char	txtBATTSETTINGS[] 	PROGMEM="Batt Settings";
+char	txtCHARGELIMITS[] 	PROGMEM="Charge Limits";
+char	txtPWM[] 	PROGMEM="Pwm";
+char	txtBALACTIVVOLTAGE[] 	PROGMEM="Bal. Activ. Voltage";
+char	txtCHARGEVOLTAGE[] 	PROGMEM="Charge Voltage";
+char	txtSTORAGEVOLTAGE[] 	PROGMEM="Storage Voltage";
+char	txtDISCHARGEVOLTAGE[] 	PROGMEM="Discharge Voltage";
+char	txtLIFEPOVOLT[] 	PROGMEM="LiFePo4 Volt.";
+char	txtLIIONVOLTAGE[] 	PROGMEM="LiIon Voltage";
+char	txtCAPACITY[] 	PROGMEM="Capacity";
+char	txtTIME[] 	PROGMEM="Time";
+char	txtFEQUENCY[] 	PROGMEM="Fequency";
+char	txtREFRESHPERIOD[] 	PROGMEM="Refresh-period";
 
 // Parameter definitions
-Parameter_t parCurrent =
-	{ 0, 100, 10000, 100, mA };
-Parameter_t parCellCount =
-	{ 0, 0, 6, 1, Cells };
-Parameter_t parLiPoChVolt =
-	{ 4150, 3000, 4200, 10, mV }; // fixme!!
-Parameter_t parLiPoStVolt =
-	{ 3800, 2000, 4100, 100, mV };
-Parameter_t parLiPoDisVolt =
-	{ 0, 3000, 4200, 10, mV };
-Parameter_t parLiFeVolt =
-	{ 0, 2000, 3700, 10, mV };
-Parameter_t parLiIonVolt =
-	{ 0, 3500, 4100, 10, mV };
-Parameter_t parBalActVolt =
-	{ 0, 0, 100, 100, proz };
-Parameter_t parMaxcap =
-	{ 0, 0, 10000, 100, mAh };
-Parameter_t parMaxtime =
-	{ 0, 1, 6000, 1, min };
-Parameter_t parPWMfrequency =
-	{ 0, 10, 100, 10, kHz };
-Parameter_t parRefreshPeriod =
-	{ 0, 1, 10000, 10, ms };
+Parameter_t parCurrent = {	0, 100, 10000, 100, mA};
+Parameter_t parCellCount = {	0, 0, 6, 1, Cells};
+Parameter_t parLiPoChVolt = {	0, 3000, 4200, 10, mV};
+Parameter_t parLiPoStVolt = {	0, 2000, 4100, 100, mV};
+Parameter_t parLiPoDisVolt = {	0, 3000, 4200, 10, mV};
+Parameter_t parLiFeVolt = {	0, 2000, 3700, 10, mV};
+Parameter_t parLiIonVolt = {	0, 3500, 4100, 10, mV};
+Parameter_t parBalActVolt = {	0, 0, 100, 100, proz};
+Parameter_t parMaxcap = {	0, 0, 10000, 100, mAh};
+Parameter_t parMaxtime = {	0, 1, 6000, 1, min};
+Parameter_t parPWMfrequency = {	0, 10, 100, 10, kHz};
+Parameter_t parRefreshPeriod = {	0, 10, 1000, 10, parts};
 
-//Name	Act	Par	Jmp	Parent	Memory
-MenuItem_t m_items[MENUESIZE] =
-	{
-	/* 0*/
-		{ txtMAINMENU, 0, 0, 1, 0, FLASH },
-	/* 1*/
-		{ txtCURRENTSETPOINT, 0, &parCurrent, 0, 0, FLASH },
-	/* 2*/
-		{ txtACTION, 0, 0, 6, 0, FLASH },
-	/* 3*/
-		{ txtBATTTYPESELECT, 0, 0, 9, 0, FLASH },
-	/* 4*/
-		{ txtSETUP, 0, 0, 12, 0, FLASH },
-	/* 5*/
-		{ txtBACK, leavemenu, 0, 0, 0, FLASH },
-	/* 6*/
-		{ txtCHARGEFULL, ActionChargeMethodFull, 0, 0, 2, FLASH },
-	/* 7*/
-		{ txtCHARGESTORAGE, ActionChargeMethodStorage, 0, 0, 2, FLASH },
-	/* 8*/
-		{ txtDISCHARGE, ActionChargeMethodDischarge, 0, 0, 2, FLASH },
-	/* 9*/
-		{ txtLIPO, actSelTypeLiPo, 0, 0, 3, FLASH },
-	/* 10*/
-		{ txtLIFEPO, actSelTypeLiFe, 0, 0, 3, FLASH },
-	/* 11*/
-		{ txtLIION, actSelTypeLiIon, 0, 0, 3, FLASH },
-	/* 12*/
-		{ txtCELLCOUNT, 0, &parCellCount, 0, 4, FLASH },
-	/* 13*/
-		{ txtBATTSETTINGS, 0, 0, 17, 4, FLASH },
-	/* 14*/
-		{ txtCHARGELIMITS, 0, 0, 30, 4, FLASH },
-	/* 15*/
-		{ txtPWM, 0, 0, 33, 4, FLASH },
-	/* 16*/
-		{ txtBACK, 0, 0, 4, 4, FLASH },
-	/* 17*/
-		{ txtLIPO, 0, 0, 22, 13, FLASH },
-	/* 18*/
-		{ txtLIFEPO, 0, 0, 26, 13, FLASH },
-	/* 19*/
-		{ txtLIION, 0, 0, 28, 13, FLASH },
-	/* 20*/
-		{ txtBALACTIVVOLTAGE, 0, &parBalActVolt, 0, 13, FLASH },
-	/* 21*/
-		{ txtBACK, 0, 0, 13, 13, FLASH },
-	/* 22*/
-		{ txtCHARGEVOLTAGE, 0, &parLiPoChVolt, 0, 17, FLASH },
-	/* 23*/
-		{ txtSTORAGEVOLTAGE, 0, &parLiPoStVolt, 0, 17, FLASH },
-	/* 24*/
-		{ txtDISCHARGEVOLTAGE, 0, &parLiPoDisVolt, 0, 17, FLASH },
-	/* 25*/
-		{ txtBACK, 0, 0, 17, 17, FLASH },
-	/* 26*/
-		{ txtLIFEPOVOLT, 0, &parLiFeVolt, 0, 18, FLASH },
-	/* 27*/
-		{ txtBACK, 0, 0, 18, 18, FLASH },
-	/* 28*/
-		{ txtLIIONVOLTAGE, 0, &parLiIonVolt, 0, 19, FLASH },
-	/* 29*/
-		{ txtBACK, 0, 0, 19, 19, FLASH },
-	/* 30*/
-		{ txtCAPACITY, 0, &parMaxcap, 0, 14, FLASH },
-	/* 31*/
-		{ txtTIME, 0, &parMaxtime, 0, 14, FLASH },
-	/* 32*/
-		{ txtBACK, 0, 0, 14, 14, FLASH },
-	/* 33*/
-		{ txtFEQUENCY, 0, &parPWMfrequency, 0, 15, FLASH },
-	/* 34*/
-		{ txtREFRESHPERIOD, 0, &parRefreshPeriod, 0, 15, FLASH },
-	/* 35*/
-		{ txtBACK, 0, 0, 15, 15, FLASH }, };
+			//Name	Act	Par	Jmp	Parent	Memory
+MenuItem_t m_items[MENUESIZE] = {
+	/* 0*/	{txtMAINMENU,	 0,	 0,	1,	0,	FLASH},
+	/* 1*/	{txtCURRENTSETPOINT,	 0,	 &parCurrent,	0,	0,	FLASH},
+	/* 2*/	{txtACTION,	 0,	 0,	6,	0,	FLASH},
+	/* 3*/	{txtBATTTYPESELECT,	 0,	 0,	9,	0,	FLASH},
+	/* 4*/	{txtSETUP,	 0,	 0,	12,	0,	FLASH},
+	/* 5*/	{txtBACK,	 leavemenu,	 0,	0,	0,	FLASH},
+	/* 6*/	{txtCHARGEFULL,	 ActionChargeMethodFull,	 0,	0,	2,	FLASH},
+	/* 7*/	{txtCHARGESTORAGE,	 ActionChargeMethodStorage,	 0,	0,	2,	FLASH},
+	/* 8*/	{txtDISCHARGE,	 ActionChargeMethodDischarge,	 0,	0,	2,	FLASH},
+	/* 9*/	{txtLIPO,	 actSelTypeLiPo,	 0,	0,	3,	FLASH},
+	/* 10*/	{txtLIFEPO,	 actSelTypeLiFe,	 0,	0,	3,	FLASH},
+	/* 11*/	{txtLIION,	 actSelTypeLiIon,	 0,	0,	3,	FLASH},
+	/* 12*/	{txtCELLCOUNT,	 0,	 &parCellCount,	0,	4,	FLASH},
+	/* 13*/	{txtBATTSETTINGS,	 0,	 0,	17,	4,	FLASH},
+	/* 14*/	{txtCHARGELIMITS,	 0,	 0,	30,	4,	FLASH},
+	/* 15*/	{txtPWM,	 0,	 0,	33,	4,	FLASH},
+	/* 16*/	{txtBACK,	 0,	 0,	4,	4,	FLASH},
+	/* 17*/	{txtLIPO,	 0,	 0,	22,	13,	FLASH},
+	/* 18*/	{txtLIFEPO,	 0,	 0,	26,	13,	FLASH},
+	/* 19*/	{txtLIION,	 0,	 0,	28,	13,	FLASH},
+	/* 20*/	{txtBALACTIVVOLTAGE,	 0,	 &parBalActVolt,	0,	13,	FLASH},
+	/* 21*/	{txtBACK,	 0,	 0,	13,	13,	FLASH},
+	/* 22*/	{txtCHARGEVOLTAGE,	 0,	 &parLiPoChVolt,	0,	17,	FLASH},
+	/* 23*/	{txtSTORAGEVOLTAGE,	 0,	 &parLiPoStVolt,	0,	17,	FLASH},
+	/* 24*/	{txtDISCHARGEVOLTAGE,	 0,	 &parLiPoDisVolt,	0,	17,	FLASH},
+	/* 25*/	{txtBACK,	 0,	 0,	17,	17,	FLASH},
+	/* 26*/	{txtLIFEPOVOLT,	 0,	 &parLiFeVolt,	0,	18,	FLASH},
+	/* 27*/	{txtBACK,	 0,	 0,	18,	18,	FLASH},
+	/* 28*/	{txtLIIONVOLTAGE,	 0,	 &parLiIonVolt,	0,	19,	FLASH},
+	/* 29*/	{txtBACK,	 0,	 0,	19,	19,	FLASH},
+	/* 30*/	{txtCAPACITY,	 0,	 &parMaxcap,	0,	14,	FLASH},
+	/* 31*/	{txtTIME,	 0,	 &parMaxtime,	0,	14,	FLASH},
+	/* 32*/	{txtBACK,	 0,	 0,	14,	14,	FLASH},
+	/* 33*/	{txtFEQUENCY,	 0,	 &parPWMfrequency,	0,	15,	FLASH},
+	/* 34*/	{txtREFRESHPERIOD,	 0,	 &parRefreshPeriod,	0,	15,	FLASH},
+	/* 35*/	{txtBACK,	 0,	 0,	15,	15,	FLASH},
+};
 
 //******** END OF AUTO-GENERATED CODE DO NOT EDIT!!! *********
 
--- LipoDisp/AVR/src/command.c
+++ LipoDisp/AVR/src/command.c
@@ -100,9 +100,6 @@ void TaskCommand(void)
 			}
 	    }
 
-
-
-
 	}
 }
 
