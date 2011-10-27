845a5e47c5786165578bf5c94e0ab1f71cd3d2ed
admin <admin@5e083c7a-a129-45ab-8a5b-46b92a8de37d>
2011-10-27 22:42:13 +0000

    Tastensperre hinzu

--- LipoDisp/AVR/touch/touchpad.c
+++ LipoDisp/AVR/touch/touchpad.c
@@ -5,12 +5,12 @@
 #include "../menu/menu.h"
 #include "../menu/menu_variant.h"
 
-
 // Prototypes:
 void ProcessTouch(void);
 int16_t GetvalueFromUI(void);
 uint8_t touchGetSpeed(int16_t* speed, int32_t *Schwerpunkt);
-void HandOverValueToUI(uint16_t value, uint16_t upper, uint16_t lower, uint16_t stepsize);
+void HandOverValueToUI(uint16_t value, uint16_t upper, uint16_t lower,
+		uint16_t stepsize);
 int32_t touchGetSchwerpunkt(void);
 void sFilter(int16_t* o, int16_t* n);
 void svFilter(int16_t* o, int16_t* n, uint8_t x);
@@ -26,7 +26,7 @@ extern ChargerMode_t g_Tansfer_Action;
 extern uint8_t g_debug, g_debug2, g_debug3;
 extern int16_t g_debug4;
 extern uint8_t g_bMenuActive;
-extern Command_t g_tCommand ; // only for adjusting the current while charging
+extern Command_t g_tCommand; // only for adjusting the current while charging
 
 int16_t g_ausTouchCalValues[5] =
 TOUCHCALINIT;
@@ -34,13 +34,11 @@ TOUCHCALINIT;
 int16_t g_aucTouchpads[TOUCHCOUNT];
 
 particle_t myP =
-	{ 0, 0, 0, 99 ,0,32000};
-
-
+	{ 0, 0, 0, 99, 0, 32000 };
 
 void TaskTouch()
 {
-	OS_SetAlarm(OSALTouchRepeat,10);
+	OS_SetAlarm( OSALTouchRepeat, 10 );
 
 #define TOUCHSENSELEVEL 15
 
@@ -49,13 +47,12 @@ void TaskTouch()
 	while(1)
 	{
 
-		OS_WaitAlarm(OSALTouchRepeat);
-		OS_SetAlarm(OSALTouchRepeat,10); // every 10ms
+		OS_WaitAlarm( OSALTouchRepeat );
+		OS_SetAlarm( OSALTouchRepeat, 10 ); // every 10ms
 
 		ProcessTouch();//change value
 	}
 }
-
 
 /*
 
@@ -79,19 +76,19 @@ void TaskTouch()
 uint8_t touchGetPad5(uint8_t pin)
 {
 	uint8_t value, i;
-	uint8_t mask = (1 << pin);
+	uint8_t mask = ( 1 << pin );
 
 	value = 0;
 	OS_DISABLEALLINTERRUPTS;// absolutely no interrupts allowed here!
-	for (i = 0; i < TOUCHREPCNT; i++)
+	for(i = 0; i < TOUCHREPCNT ; i++)
 	{
 		TOUCHTOGGLEHIGH;
-		while (!(TOUCHPORT.IN & mask))
+		while(!( TOUCHPORT.IN & mask ))
 		{
 			value++;
 		}
 		TOUCHTOGGLELOW;
-		while (TOUCHPORT.IN & mask)
+		while(TOUCHPORT.IN & mask)
 		{
 			value++;
 		}
@@ -99,9 +96,9 @@ uint8_t touchGetPad5(uint8_t pin)
 	OS_ENABLEALLINTERRUPTS;
 
 	// limit re-calibration
-	if(value*100 < g_ausTouchCalValues[pin]) // wenns denn schneller war
+	if(value * 100 < g_ausTouchCalValues[pin]) // wenns denn schneller war
 	{
-		if(value*100 < g_ausTouchCalValues[pin] - 500) //ups das war aber zu flott
+		if(value * 100 < g_ausTouchCalValues[pin] - 500) //ups das war aber zu flott
 		{
 			// lassen wie's war bzw. nur leicht weniger
 			g_ausTouchCalValues[pin] -= 20;
@@ -109,50 +106,48 @@ uint8_t touchGetPad5(uint8_t pin)
 		else
 		{
 			// mitnehmen
-			g_ausTouchCalValues[pin] = value*100;
+			g_ausTouchCalValues[pin] = value * 100;
 		}
 	}
 
 	g_ausTouchCalValues[pin]++; // every some time, correct the calibration bytes.. even, if a touch is recognized... -> provides self healing..
 
 
-
-	return value - (g_ausTouchCalValues[pin] / 100);
+	return value - ( g_ausTouchCalValues[pin] / 100 );
 }
-
 
 #define MOMENTMULTIPLIER 100ULL
 
 int32_t touchGetSchwerpunkt(void)
 {
-int32_t sum;
-int32_t Moment;
-int32_t ret;
+	int32_t sum;
+	int32_t Moment;
+	int32_t ret;
 	uint8_t i;
 
-	for (i = 0; i < TOUCHCOUNT; i++)
+	for(i = 0; i < TOUCHCOUNT ; i++)
 	{
-		g_aucTouchpads[i] = touchGetPad5(i);
+		g_aucTouchpads[i] = touchGetPad5( i );
 	}
 
 	Moment = 0;
 	sum = 0;
-	for (i = 0; i < TOUCHCOUNT; i++)
+	for(i = 0; i < TOUCHCOUNT ; i++)
 	{
 
-		Moment += (i + 1) * MOMENTMULTIPLIER * g_aucTouchpads[i];
+		Moment += ( i + 1 ) * MOMENTMULTIPLIER * g_aucTouchpads[i];
 		sum += g_aucTouchpads[i];
 	}
-	if (sum > (SLIDEMINSIGNAL)) //fixme make self learning
+	if(sum > ( SLIDEMINSIGNAL )) //fixme make self learning
 	{
-		ret = (Moment / sum) - MOMENTMULTIPLIER;
+		ret = ( Moment / sum ) - MOMENTMULTIPLIER;
 
 		// fixme debug:
 		if(ret < 0)
 		{
 			asm("break");
 		}
-		return ret ;
+		return ret;
 	}
 	else
 		return -1;
@@ -162,33 +157,34 @@ int32_t ret;
 void sFilter(int16_t* o, int16_t* n)
 {
 	int32_t temp;
-	if (*o == 0)
+	if(*o == 0)
 		*o = *n;
 	else
 	{
 		temp = *o * 3 + *n;
-		*o = (temp + 2) / 4; // "halbes dazu, wg Rundungsfehler"
+		*o = ( temp + 2 ) / 4; // "halbes dazu, wg Rundungsfehler"
 	}
 }
 
 void svFilter(int16_t* o, int16_t* n, uint8_t x)
 {
 	int32_t temp;
-	if (*o == 0)
+	if(*o == 0)
 		*o = *n;
 	else
 	{
-		temp = *o * (x - 1) + *n;
-		*o = (temp + x / 2) / x; // "halbes dazu, wg Rundungsfehler"
+		temp = *o * ( x - 1 ) + *n;
+		*o = ( temp + x / 2 ) / x; // "halbes dazu, wg Rundungsfehler"
 	}
 }
 
 #define SLIDERUPSCALE 32000ul
 
-void HandOverValueToUI(uint16_t value, uint16_t upper, uint16_t lower, uint16_t stepsize) 
+void HandOverValueToUI(uint16_t value, uint16_t upper, uint16_t lower,
+		uint16_t stepsize)
 {
-	myP.upscale = (uint32_t)SLIDERUPSCALE / ((uint32_t)upper-(uint32_t)lower);
-
+	myP.upscale = (uint32_t)SLIDERUPSCALE
+			/ ( (uint32_t)upper - (uint32_t)lower );
 
 	myP.position = (uint32_t)value * myP.upscale;
 	myP.stepsize = (uint32_t)stepsize * myP.upscale;
@@ -198,7 +194,7 @@ void HandOverValueToUI(uint16_t value, uint16_t upper, uint16_t lower, uint16_t 
 
 int16_t GetvalueFromUI(void)
 {
-	return  myP.position / myP.upscale;
+	return myP.position / myP.upscale;
 }
 
 void touchGetValue(int16_t* pValue) // read txtback the (changed) value Mutex?
@@ -224,19 +220,19 @@ void touchGetValue(int16_t* pValue) // read txtback the (changed) value Mutex?
 uint8_t touchGetSpeed(int16_t* speed, int32_t *Schwerpunkt)
 {
 
-// fixme mach rein TOUCHMINDIST
+	// fixme mach rein TOUCHMINDIST
 
-	static int16_t opos=-1, fpos=-1;
+	static int16_t opos = -1, fpos = -1;
 	int16_t pos;
 	int16_t speed_raw;
 
 	*Schwerpunkt = pos = touchGetSchwerpunkt();
-	if (pos != -1)
+	if(pos != -1)
 	{
-		if (fpos == -1)
+		if(fpos == -1)
 			fpos = pos; // filter überschreiben, wenn gerade gültig geworden.
 		else
-			svFilter(&fpos, &pos, 8); // sonst filtern
+			svFilter( &fpos, &pos, 8 ); // sonst filtern
 	}
 	else
 	{
@@ -247,9 +243,9 @@ uint8_t touchGetSpeed(int16_t* speed, int32_t *Schwerpunkt)
 		return 0; // no speed!
 	}
 
-	if (opos == -1) // gerade gültig geworden?
+	if(opos == -1) // gerade gültig geworden?
 	{
-		
+
 		opos = fpos;
 		*speed = 0;
 		return 0; // no speed, neue Messung.
@@ -257,7 +253,7 @@ uint8_t touchGetSpeed(int16_t* speed, int32_t *Schwerpunkt)
 	else
 	{
 		speed_raw = fpos - opos;
-		svFilter(speed, &speed_raw, 4);
+		svFilter( speed, &speed_raw, 4 );
 
 		if(abs(*speed) > MAXSLIDESPEED || abs(*speed) < MINSLIDESPEED)
 		{
@@ -275,82 +271,83 @@ uint8_t touchGetSpeed(int16_t* speed, int32_t *Schwerpunkt)
 
 }
 
-
 eTouchstate_t eTouchstate = eTSIdle;
+uint8_t s_ucKeyLock = 0;
+uint8_t info = 0;
 
-uint8_t info=0;
-
-
-	int32_t Schwerpunkt;
+int32_t Schwerpunkt;
 void ProcessTouch(void)
 {
 
-	uint8_t ucActualGesture;
+	eGestures_t eActualGesture;
 	static int16_t s_sSpeedFiltered = 0;
-	static uint8_t s_ucOldGesture;
+	static eGestures_t s_ucOldGesture;
 	uint8_t bMoved;
 
+	
 
 	static int32_t OldSchwerpunkt;
 
-
-	bMoved = touchGetSpeed(&s_sSpeedFiltered, &Schwerpunkt/*, &TouchBitfield*/); 
+	bMoved	= touchGetSpeed( &s_sSpeedFiltered, &Schwerpunkt/*, &TouchBitfield*/);
 	// fixme  beim integrieren den Schwerpunkt / speed  nur rechnen, wenn nur ein bzw. zwei benachbarte gedrückt.
-	ucActualGesture = getGestureSkip();
+	eActualGesture = getGestureSkip();
 
 	g_debug4 = s_sSpeedFiltered;
 
-	static uint16_t TimeDiff=0;
+	static uint16_t TimeDiff = 0;
 
 	TimeDiff++;
 
-
-	switch (eTouchstate)
+	switch(eTouchstate)
 	{
 		case eTSIdle:
 			//s_sSpeedFiltered = 0;
 			myP.force = 0;
-			if (bMoved == 0 && ucActualGesture == 0)
+			if(bMoved == 0 && eActualGesture == 0)
 			{
 				// not touched
 				break;
 			}
 			else
 			{
-				TimeDiff =0;
+				TimeDiff = 0;
 				OldSchwerpunkt = Schwerpunkt;
 				eTouchstate = eTSTouching;
 			}
 			// no break;
 		case eTSTouching:
-			if (bMoved == 1) // touched / moved
+
+			if(bMoved == 1) // touched / moved
 			{
+				if(s_ucKeyLock)
+				break;
+				
 				// adapt speed
 
-				myP.force = - s_sSpeedFiltered;
+				myP.force = -s_sSpeedFiltered;
 				info = 2;
 
 				myP.velocity = myP.velocity + myP.force;
 
 				/*static uint8_t n;  // bremsen!!!
-				if (n++ == 10)
-				{
-					n = 0;
-					myP.velocity = (myP.velocity * myP.friction) / 100;
-				}*/
+				 if (n++ == 10)
+				 {
+				 n = 0;
+				 myP.velocity = (myP.velocity * myP.friction) / 100;
+				 }*/
 			}
 			else
-			{	
-				if (myP.velocity == 0 )
+			{
+				if(myP.velocity == 0)
 				{
-						eTouchstate = eTSGesture;
-						info = 3;
+					eTouchstate = eTSGesture;
+					info = 3;
 				}
 				else
 				{
 					// do nothing and let particle move on
 					info = 5;
-					if(ucActualGesture != 0)
+					if(eActualGesture != 0)
 					{
 						// bremsen
 						myP.velocity = 0;
@@ -369,12 +366,15 @@ void ProcessTouch(void)
 
 		case eTSGesture:
 			info = 7;
-			switch (s_ucOldGesture) // können wir nehmen, weil lag lange genug an.
+			switch(s_ucOldGesture)
+			// können wir nehmen, weil lag lange genug an.
 			{
 				case eGNothing:
 
 					break;
 				case eGPlus:
+					if(s_ucKeyLock)
+						break;
 					if(g_bMenuActive)
 					{
 						if(myP.position + myP.stepsize < myP.max)
@@ -384,13 +384,16 @@ void ProcessTouch(void)
 					}
 					else
 					{
-						OS_MutexGet(OSMTXCommand);
-						if(g_tCommand.sCurrentSetpoint + 100 < parCurrent.sUpperLimit)
+						OS_MutexGet( OSMTXCommand );
+						if(g_tCommand.sCurrentSetpoint + 100
+								< parCurrent.sUpperLimit)
 							g_tCommand.sCurrentSetpoint += 100;
-						OS_MutexRelease(OSMTXCommand);
+						OS_MutexRelease( OSMTXCommand );
 					}
 					break;
 				case eGMitte:
+					if(s_ucKeyLock)
+						break;
 					// menue bestätigung
 					if(g_bMenuActive)
 					{
@@ -398,7 +401,7 @@ void ProcessTouch(void)
 					}
 					else
 					{
-						if(GetBattStatus() == eBattWaiting )
+						if(GetBattStatus() == eBattWaiting)
 						{
 							// fixme duplicate weil der doppeltatsch net geht.
 
@@ -407,9 +410,10 @@ void ProcessTouch(void)
 
 							uint8_t SubMenuGroupSize, StartIndex;
 
-							GetSubMenuCount(&SubMenuGroupSize, &StartIndex);
+							GetSubMenuCount( &SubMenuGroupSize, &StartIndex );
 
-							HandOverValueToUI(SubMenuGroupSize, SubMenuGroupSize, 1, 1);
+							HandOverValueToUI( SubMenuGroupSize,
+									SubMenuGroupSize, 1, 1 );
 
 							g_bMenuActive = 1;
 						}
@@ -422,6 +426,8 @@ void ProcessTouch(void)
 
 					break;
 				case eGMinus:
+					if(s_ucKeyLock)
+						break;
 					if(g_bMenuActive)
 					{
 						if(myP.position - myP.stepsize > myP.min)
@@ -431,26 +437,22 @@ void ProcessTouch(void)
 					}
 					else
 					{
-						OS_MutexGet(OSMTXCommand);
-						if(g_tCommand.sCurrentSetpoint - 100 > parCurrent.sLowerLimit)
-						 g_tCommand.sCurrentSetpoint -= 100;
-						OS_MutexRelease(OSMTXCommand);
+						OS_MutexGet( OSMTXCommand );
+						if(g_tCommand.sCurrentSetpoint - 100
+								> parCurrent.sLowerLimit)
+							g_tCommand.sCurrentSetpoint -= 100;
+						OS_MutexRelease( OSMTXCommand );
 					}
 					break;
 				case eGSplit:
 					// unlock
-					if(g_bMenuActive)
+					if(s_ucKeyLock)
 					{
-						// nix, weil im menu gibt's nen "back"
+						s_ucKeyLock = 0;
 					}
 					else
 					{
-//						geht nicht, so machen wie anderer Stelle!!
-//						GetSubMenuCount(&SubMenuGroupSize, &StartIndex);
-//
-//						HandOverValueToUI(SubMenuGroupSize, SubMenuGroupSize, 1, 1);
-//
-//						g_bMenuActive = 1;
+						s_ucKeyLock = 1;
 					}
 
 					break;
@@ -466,13 +468,13 @@ void ProcessTouch(void)
 			g_debug2 = s_ucOldGesture;
 
 			eTouchstate = eTSBlocked;
-			TimeDiff =0;
+			TimeDiff = 0;
 
 			//OS_WaitTicks(OSALTouchPause,10);
 			break;
 
 		case eTSBlocked:
-			if (bMoved == 0 && TimeDiff > 20 && ucActualGesture == 0) // warte bis komplett losgelassen
+			if(bMoved == 0 && TimeDiff > 20 && eActualGesture == 0) // warte bis komplett losgelassen
 			{
 				eTouchstate = eTSIdle;
 			}
@@ -483,11 +485,11 @@ void ProcessTouch(void)
 			break;
 	}
 
-	s_ucOldGesture = ucActualGesture;
+	s_ucOldGesture = eActualGesture;
 
 	g_debug = eTouchstate;
 
-	g_debug3 = ucActualGesture;
+	g_debug3 = eActualGesture;
 
 }
 
@@ -495,11 +497,11 @@ eGestures_t getGesture(void) // delayed by one cycle
 {
 	uint8_t ret = 0;
 	uint8_t i;
-	
-	for (i=0; i < TOUCHCOUNT; i++)
+
+	for(i = 0; i < TOUCHCOUNT ; i++)
 	{
-		if (g_aucTouchpads[i] > TOUCHMINSIGNAL)
-			ret |= (1 << i);
+		if(g_aucTouchpads[i] > TOUCHMINSIGNAL)
+			ret |= ( 1 << i );
 	}
 
 	return ret; // scho feddisch
@@ -508,25 +510,24 @@ eGestures_t getGesture(void) // delayed by one cycle
 eGestures_t getGestureSkip(void) // delayed by one cycle
 {
 	uint8_t ret = 0;
-	uint8_t i,j;
+	uint8_t i, j;
 
-	for (i=0, j=0; i < TOUCHCOUNT; i+=2,j++) // nur die obere mittlere und untere zulassen.
+	for(i = 0 , j = 0; i < TOUCHCOUNT ; i += 2 , j++) // nur die obere mittlere und untere zulassen.
 	{
-		if (g_aucTouchpads[i] > TOUCHMINSIGNAL)
-			ret |= (1 << j);
+		if(g_aucTouchpads[i] > TOUCHMINSIGNAL)
+			ret |= ( 1 << j );
 	}
 
-	return ret ; 
+	return ret;
 }
-
 
 uint8_t bitcount(uint8_t b)
 {
-	uint8_t r=0,i;
+	uint8_t r = 0, i;
 
-	for (i = 0; i < 8; ++i)
+	for(i = 0; i < 8 ; ++i)
 	{
-		if(b & (1<<i))
+		if(b & ( 1 << i ))
 			r++;
 	}
 	return r;
@@ -534,17 +535,15 @@ uint8_t bitcount(uint8_t b)
 
 uint8_t bitcount3(uint8_t b)
 {
-	uint8_t r=0,i;
+	uint8_t r = 0, i;
 
-	for (i = 0; i < 3; ++i)
+	for(i = 0; i < 3 ; ++i)
 	{
-		if(b & (1<<i))
+		if(b & ( 1 << i ))
 			r++;
 	}
 	return r;
 }
-
-
 
 void touchtest(void)
 {
@@ -581,17 +580,17 @@ void touchtest(void)
 
 	lcd_draw_pixel( RED,g,  (-myP.position/0xff)+160);
 	//lcd_draw_pixel(GREEN,g,myP.velocity/0xff+128);
-	//lcd_draw_pixel(YELLOW,g,myP.force/0xff+160);
-	lcd_draw_pixel(WHITE,g,30-g_debug*5);
-	if (g == 320)
-	{
-		lcd_clear();
-		lcd_draw_line(YELLOW,0,30,320,30);
-		g =0;
-	}
+			//lcd_draw_pixel(YELLOW,g,myP.force/0xff+160);
+			lcd_draw_pixel(WHITE,g,30-g_debug*5);
+			if (g == 320)
+			{
+				lcd_clear();
+				lcd_draw_line(YELLOW,0,30,320,30);
+				g =0;
+			}
 
-	lcd_print(WHITE, BLACK, 1, 0, 32,"State: %i  " ,(uint16_t)g_debug);
-	lcd_print(WHITE, BLACK, 1, 0, 64,"Gesture: %i  " ,(uint16_t)g_debug2);
+			lcd_print(WHITE, BLACK, 1, 0, 32,"State: %i  " ,(uint16_t)g_debug);
+			lcd_print(WHITE, BLACK, 1, 0, 64,"Gesture: %i  " ,(uint16_t)g_debug2);
 	lcd_print(WHITE, BLACK, 1, 0, 96,"Particle: %i , %i   " ,(uint16_t)myP.position,(uint16_t)myP.velocity);
 	lcd_print(WHITE, BLACK, 1, 0, 128,"info: %i  " ,(uint16_t)info);
 
@@ -611,5 +610,4 @@ void touchtest(void)
 
 
 }
-
 
