4229026ba0723eb95135394ebddaae0aeb01a088
admin <admin@5e083c7a-a129-45ab-8a5b-46b92a8de37d>
2012-05-18 19:55:29 +0000

    UNTESTED Balancer optimisation (end of Balancing)

--- LipoLoader/AVR/balancer.c
+++ LipoLoader/AVR/balancer.c
@@ -6,20 +6,20 @@
 
 extern Battery_Info_t g_tBattery_Info; // todo avoid
 
-extern Command_t g_tCommand ;
+extern Command_t g_tCommand;
 
 typedef struct ADC_Values_tag
 {
-	int16_t		TempInt;
-	int16_t		VCC_mVolt;
-	int16_t 	Bandgap;
-	int16_t		TempCPU;
-}ADC_Values_t;
+	int16_t TempInt;
+	int16_t VCC_mVolt;
+	int16_t Bandgap;
+	int16_t TempCPU;
+} ADC_Values_t;
 
 #define BALANCEFINISHCOUNT 15
 
 uint8_t bBalancerOverload; // True, if one cell has reached Voltage limit.
-uint8_t ucBalancerFinished = BALANCEFINISHCOUNT;  // 0 indicated balancer is finished
+uint8_t ucBalancerFinished = BALANCEFINISHCOUNT; // 0 indicated balancer is finished
 //uint8_t aucDisChTicks[6]; // increased for each discharge cycle
 uint32_t aunCharge_mAs[6]; // accumulated charge
 ADC_Values_t g_tADCValues;
@@ -27,7 +27,6 @@ ADC_Values_t g_tADCValues;
 #define ADCWAITTIME 2
 #define BALANCEREPEATTIME 330L
 #define BALANCEEVERYNCYCLES 1  // 1= every second time
-
 uint8_t g_ucCalCommand = 0;
 
 void sFilterBalancer(int16_t* o, int16_t* n) // with jump possibility, if filtered value is off by more than 10%
@@ -56,14 +55,15 @@ void TaskBalance(void)
 	int16_t sBalanceCellsRaw[6]; // quasi static
 	int16_t sBalanceCells[6]; // quasi static
 	uint8_t ucBalanceBits = 0;
-	uint8_t i,j;
+	uint8_t i, j;
 	// balancing allowed
 	static uint8_t onlyEveryNCycles = 0; // 0 = Balancer is possibly on; value >0 => guaranteed to be off.
 
 	// direction for balancer pins
 	PORTC.DIRSET = 0b11111100;
 
-	OS_WaitTicks(OSALMBalRepeat,100); // wait for ADC init
+	OS_WaitTicks( OSALMBalRepeat, 100 );
+	// wait for ADC init
 
 	ADC_StartConvCh3Pin( 0 );
 
@@ -71,39 +71,38 @@ void TaskBalance(void)
 	{
 
 		OS_WaitAlarm( OSALMBalRepeat );
-		OS_SetAlarm( OSALMBalRepeat, BALANCEREPEATTIME);
+		OS_SetAlarm( OSALMBalRepeat, BALANCEREPEATTIME );
 
-		if (onlyEveryNCycles > 0)  // measurement not poisoned by balancer
+		if(onlyEveryNCycles > 0) // measurement not poisoned by balancer
 		{
-		/*for(j = 0; j < 10 ; j++) // fixme do conversion 10 times and filter...
-		{*/
-			for(i = 1; i < 6 ; i++)
+			for(j = 0; j < 8 ; j++) // do conversion 8 times and filter
 			{
-				ADC_StartConvCh3Pin( i );
-				OS_WaitTicks(OSALMBalWait,ADCWAITTIME);
-				// push voltage of channel into array
+				for(i = 1; i < 6 ; i++)
+				{
+					ADC_StartConvCh3Pin( i );
+					OS_WaitTicks( OSALMBalWait, ADCWAITTIME );
+					// push voltage of channel into array
+					sTemp = ADC_ScaleCell_mV( ADCA.CH3.RES );
+
+					sFilterBalancer( &sBalanceCellsRaw[i], &sTemp );
+				}
+
+				ADC_StartConvCh3Pin( 10 );
+				OS_WaitTicks( OSALMBalWait, ADCWAITTIME );
 				sTemp = ADC_ScaleCell_mV( ADCA.CH3.RES );
-
-				sFilterBalancer( &sBalanceCellsRaw[i], &sTemp );
+				sFilterBalancer( &sBalanceCellsRaw[0], &sTemp );
 			}
-
-			ADC_StartConvCh3Pin( 10 );
-			OS_WaitTicks(OSALMBalWait,ADCWAITTIME);
-			sTemp = ADC_ScaleCell_mV( ADCA.CH3.RES );
-			sFilterBalancer( &sBalanceCellsRaw[0], &sTemp );
-		/*}*/
-
 		}
 
 		ADC_StartConvCh3Pin( 11 ); // temperature external2
-		OS_WaitTicks(OSALMBalWait,ADCWAITTIME);
+		OS_WaitTicks( OSALMBalWait, ADCWAITTIME );
 		nTemp = ADCA.CH3.RES;
 		OS_DISABLEALLINTERRUPTS;
 		g_tADCValues.TempInt = nTemp; // fixme scaling!
 		OS_ENABLEALLINTERRUPTS;
 
 		ADC_StartConvInt( 0 ); // CPU temperature
-		OS_WaitTicks(OSALMBalWait,ADCWAITTIME);
+		OS_WaitTicks( OSALMBalWait, ADCWAITTIME );
 		nTemp = ADCA.CH3.RES * 10 / 33; // what would be measured, if it was done at 1V ref (/ 3.3).
 		nTemp = ( nTemp * ( 273ul + 85ul ) / g_tCalibration.usCPUTemp85C );
 		OS_DISABLEALLINTERRUPTS;
@@ -111,17 +110,18 @@ void TaskBalance(void)
 		OS_ENABLEALLINTERRUPTS;
 
 		ADC_StartConvInt( 1 ); // Bandgap reference
-		OS_WaitTicks(OSALMBalWait,ADCWAITTIME);
+		OS_WaitTicks( OSALMBalWait, ADCWAITTIME );
 		OS_DISABLEALLINTERRUPTS;
 		g_tADCValues.Bandgap = ADCA.CH3.RES; // bit value for 1.10V ! at ref = Usupp/1.6
 		OS_ENABLEALLINTERRUPTS;
 		nTemp = ( 2048ul * 1105ul ) / g_tADCValues.Bandgap; // 1088ul old value // by knowing, that the voltage is 1.088V, we calculate the ADCRef voltage.
+		sTemp = nTemp;
 		OS_DISABLEALLINTERRUPTS;
-		sFilter(&g_tCalibration.sADCRef_mV , &nTemp);
+		sFilter( &g_tCalibration.sADCRef_mV, &sTemp );
 		OS_ENABLEALLINTERRUPTS;
 
 		ADC_StartConvInt( 2 ); // VCC_mVolt measurement
-		OS_WaitTicks(OSALMBalWait,ADCWAITTIME);
+		OS_WaitTicks( OSALMBalWait, ADCWAITTIME );
 		nTemp = ADCA.CH3.RES * 10ul;
 		nTemp = nTemp * g_tCalibration.sADCRef_mV / 2048ul;
 
@@ -131,7 +131,8 @@ void TaskBalance(void)
 
 		// end of adc conversons
 
-		switch (g_ucCalCommand) {
+		switch(g_ucCalCommand)
+		{
 			case 1:
 				CalCellsLow( &sBalanceCellsRaw[0] );
 				g_ucCalCommand = 0;
@@ -145,7 +146,7 @@ void TaskBalance(void)
 		}
 
 		// ok, now we have all cells, now apply the calibration
-		CalibrateCells(&sBalanceCellsRaw[0], &sBalanceCells[0]);
+		CalibrateCells( &sBalanceCellsRaw[0], &sBalanceCells[0] );
 
 		// mean Cell Voltage
 		int16_t mean = 0;
@@ -165,50 +166,48 @@ void TaskBalance(void)
 		}
 		mean = mean / g_tBattery_Info.ucNumberOfCells;
 
-
-		if(bBalance == 1 && g_tBattery_Info.eState == eBattCharging && onlyEveryNCycles == BALANCEEVERYNCYCLES)
+		if(bBalance
+				== 1&& g_tBattery_Info.eState == eBattCharging && onlyEveryNCycles == BALANCEEVERYNCYCLES){
+		uint8_t balact=0;
+		// Balancer logic
+		for(i = 0; i < 6; i++)// process all cells
 		{
-			uint8_t balact=0;
-			// Balancer logic
-			for(i = 0; i < 6 ; i++) // process all cells
+			if(sBalanceCells[i] > mean+3 )
 			{
-				if(sBalanceCells[i] > mean+3 )
-				{
-					// switch on Balancer for this cell
-					PORTC.OUTSET = ( 1 << ( 2 + i ) );
-					ucBalanceBits |= ( 1 << i );
+				// switch on Balancer for this cell
+				PORTC.OUTSET = ( 1 << ( 2 + i ) );
+				ucBalanceBits |= ( 1 << i );
 
-
-					int32_t curr_mA;
-					curr_mA = sBalanceCells[i] / 22; // 22 Ohms
-					aunCharge_mAs[i] = (int32_t)curr_mA * BALANCEREPEATTIME / 1000L; // add the mAs produced in this cycle
-					balact=1;
-				}
-				else
-				{
-					// switch off Balancer for this cell
-					PORTC.OUTCLR = ( 1 << ( 2 + i ) );
-					ucBalanceBits &= ~( 1 << i );
-				}
-			}
-			if (balact)
-			{
-				ucBalancerFinished = BALANCEFINISHCOUNT;
+				int32_t curr_mA;
+				curr_mA = sBalanceCells[i] / 22;// 22 Ohms
+				aunCharge_mAs[i] = (int32_t)curr_mA * BALANCEREPEATTIME / 1000L;// add the mAs produced in this cycle
+				balact=1;
 			}
 			else
 			{
-				if(ucBalancerFinished > 0) ucBalancerFinished--;
+				// switch off Balancer for this cell
+				PORTC.OUTCLR = ( 1 << ( 2 + i ) );
+				ucBalanceBits &= ~( 1 << i );
 			}
-			onlyEveryNCycles = 0;
+		}
+		if (balact)
+		{
+			ucBalancerFinished = BALANCEFINISHCOUNT;
 		}
 		else
 		{
-			// balancer off
-			PORTC.OUTCLR = (0b111111<<2);
-			ucBalanceBits = 0;
-			if(onlyEveryNCycles<BALANCEEVERYNCYCLES)
-				onlyEveryNCycles++;
+			if(ucBalancerFinished > 0) ucBalancerFinished--;
 		}
+		onlyEveryNCycles = 0;
+	}
+	else
+	{
+		// balancer off
+		PORTC.OUTCLR = (0b111111<<2);
+		ucBalanceBits = 0;
+		if(onlyEveryNCycles<BALANCEEVERYNCYCLES)
+		onlyEveryNCycles++;
+	}
 
 		// calculate overvolt protection per cell
 		uint8_t j = 0;
@@ -219,7 +218,7 @@ void TaskBalance(void)
 				j++;
 			}
 		}
-		if(j>0)
+		if(j > 0)
 		{
 			bBalancerOverload = 1;
 		}
@@ -227,7 +226,6 @@ void TaskBalance(void)
 		{
 			bBalancerOverload = 0;
 		}
-
 
 		// copy stuff for other tasks
 		OS_MutexGet( OSMTXBattInfo );
@@ -237,15 +235,15 @@ void TaskBalance(void)
 			// Copy voltage stuff
 			g_tBattery_Info.Cells[i].sVoltage_mV = sBalanceCells[i];
 			g_tBattery_Info.Cells[i].unDisCharge_mAs += aunCharge_mAs[i];
-			aunCharge_mAs[i]=0;
+			aunCharge_mAs[i] = 0;
 		}
 		OS_MutexRelease( OSMTXBattInfo );
 
 		//StateMachineBattery();
 
-		OS_SetEvent( OSTSKState, OSEVTState); // do in separate task to stay in pace.
+		OS_SetEvent( OSTSKState, OSEVTState ); // do in separate task to stay in pace.
 
-	}// while(1)
+	} // while(1)
 }
 
 uint8_t Balancer_GetOverload(void)
@@ -255,5 +253,5 @@ uint8_t Balancer_GetOverload(void)
 
 uint8_t Balancer_GetFinished(void)
 {
-	return ucBalancerFinished=0?1:0;
+	return ucBalancerFinished = 0 ? 1 : 0;
 }
