c0a3af1e189638be33eb33dd243b492321827dfd
Uli <Uli@5e083c7a-a129-45ab-8a5b-46b92a8de37d>
2016-05-29 20:38:29 +0000

    Viele Kleinigkeiten geändert: -es gibt mehr Presets und sie lassen sich jetzt abspeichern -Abschaltverhalten verändert -Logik für laden ohne Balancer angepasst -KI-Wert des Soll-Ladestroms von 8000 auf 10000 geändert -> etwas schneller ...

--- LipoDisp/AVR/menu/menu_variant.h
+++ LipoDisp/AVR/menu/menu_variant.h
@@ -24,9 +24,9 @@ extern void menu_draw_groupposition(uint8_t itemnr, uint8_t groupitems);
 //******** START OF AUTO-GENERATED HEADER DO NOT EDIT!!! *********
 //********          Generated with Treebuilder.exe       *********
 
- #define	MENUESIZE	44	// number of menu itmes (array size)
- #define	MAX_ITEM_NAME_CHARLENGTH	24	// max name length
- #define	MENUGENUID	55734	// Generation UID
+ #define	MENUESIZE	56	// number of menu itmes (array size)
+ #define	MAX_ITEM_NAME_CHARLENGTH	20	// max name length
+ #define	MENUGENUID	9889	// Generation UID
 // Enum definitions
 typedef enum
 {
@@ -46,29 +46,39 @@ typedef struct myPar_tag
       Parameter_t parBalActVoltSET;	// 
       Parameter_t parMaxcap;	// 
       Parameter_t parMaxtime;	// 
-      Parameter_t parChVoltLiPo;	// 
-      Parameter_t parBalActVoltLiPo;	// 
+      Parameter_t parChVoltLiPo1;	// 
+      Parameter_t parBalActVoltLiPo1;	// 
+      Parameter_t parChVoltLiPo2;	// 
+      Parameter_t parBalActVoltLiPo2;	// 
       Parameter_t parVoltLiIo;	// 
       Parameter_t parBalActVoltLiIo;	// 
       Parameter_t parVoltLiFe;	// 
       Parameter_t parBalActVoltLiFe;	// 
-      Parameter_t parChtVoltStorage;	// 
-      Parameter_t parBalActVoltStor;	// 
-      Parameter_t parVoltDisch;	// 
+      Parameter_t parChtVoltStorage1;	// 
+      Parameter_t parBalActVoltStor1;	// 
+      Parameter_t parChtVoltStorage2;	// 
+      Parameter_t parBalActVoltStor2;	// 
       Parameter_t parRefreshPeriod;	// 
       Parameter_t parMinSupplyVolt;	// 
 } myPar_t;
 
 extern myPar_t myPar;
 
- #define	MENUE_PARCOUNT	16	// number of parameters
+ #define	MENUE_PARCOUNT	19	// number of parameters
 
 // Action Prototypes
-void ActionChargeModeStorage (void);	// 
-void ActionChargeModeLiPo (void);	// 
-void ActionChargeModeLiIo (void);	// 
+void ActionChargeModeStorage1 (void);	// 
+void ActionChargeModeLiPo1 (void);	// 
+void ActionChargeModeStorage2 (void);	// 
 void ActionChargeModeLiFe (void);	// 
-void ActionChargeModeDischarge (void);	// 
+void ActionChargeModeLiIo (void);	// 
+void ActionChargeModeLiPo2 (void);	// 
+void savePreset (void);	// 
+void savePreset (void);	// 
+void savePreset (void);	// 
+void savePreset (void);	// 
+void savePreset (void);	// 
+void savePreset (void);	// 
 void actCalH (void);	// 
 void actCalL (void);	// 
 void ActionShowLast (void);	// 
@@ -78,18 +88,19 @@ void ActionShowLast (void);	//
 #define MENUE_TEXT_VARDEF \
 char	txtREADY[] 	PROGMEM="Ready..."; \
 char	txtPRESETS[] 	PROGMEM="Presets"; \
-char	txtCURRENTSETPOINTMA[] 	PROGMEM="Current Setpoint [mA]"; \
-char	txtCELLVOLTAGESETMV[] 	PROGMEM="Cell Voltage SET [mV]"; \
-char	txtBALACTIVVOLTAGEMV[] 	PROGMEM="Bal. Activ. Voltage [mV]"; \
-char	txtMAXCAPACITYMAH[] 	PROGMEM="Max Capacity [mAh]"; \
-char	txtMAXTIMEMIN[] 	PROGMEM="Max Time [min]"; \
+char	txtCURRENTSETPOINT[] 	PROGMEM="Current Setpoint"; \
+char	txtCELLVOLTAGESET[] 	PROGMEM="Cell Voltage SET"; \
+char	txtBALACTIVVOLTAGE[] 	PROGMEM="Bal. Activ. Voltage"; \
+char	txtMAXCAPACITY[] 	PROGMEM="Max Capacity"; \
+char	txtMAXTIME[] 	PROGMEM="Max Time"; \
 char	txtSETUP[] 	PROGMEM="Setup"; \
 char	txtSHOWLASTVALUES[] 	PROGMEM="Show Last Values"; \
 char	txtSTORAGE[] 	PROGMEM="Storage"; \
 char	txtCHARGELIPO[] 	PROGMEM="Charge LiPo"; \
-char	txtCHARGELIIO[] 	PROGMEM="Charge LiIo"; \
+char	txtSTORAGEHIGH[] 	PROGMEM="Storage high"; \
 char	txtCHARGELIFE[] 	PROGMEM="Charge LiFe"; \
-char	txtDISCHARGE[] 	PROGMEM="Discharge"; \
+char	txtCHARGELIIO[] 	PROGMEM="Charge LiIo"; \
+char	txtCHARGELIPOLOW[] 	PROGMEM="Charge LiPo low"; \
 char	txtBACK[] 	PROGMEM="(back)"; \
 char	txtPRESETSETUP[] 	PROGMEM="Preset Setup"; \
 char	txtPWM[] 	PROGMEM="Pwm"; \
@@ -97,15 +108,17 @@ char	txtMINSUPPLY[] 	PROGMEM="Min Supply"; \
 char	txtCALIBRATIONH[] 	PROGMEM="Calibration H"; \
 char	txtCALIBRATIONL[] 	PROGMEM="Calibration L"; \
 char	txtLITHIUMPOLYMER[] 	PROGMEM="Lithium Polymer"; \
+char	txtLITHIUMPOLYMERLOW[] 	PROGMEM="Lithium Polymer low"; \
 char	txtLITHIUMION[] 	PROGMEM="Lithium Ion"; \
 char	txtLIFEPO[] 	PROGMEM="LiFePo4"; \
 char	txtBACKTOMAIN[] 	PROGMEM="(back to Main)"; \
 char	txtLIPOVOLTAGE[] 	PROGMEM="LiPo Voltage"; \
-char	txtBALACTIVVOLTAGE[] 	PROGMEM="Bal. Activ. Voltage"; \
+char	txtBACKANDSAVE[] 	PROGMEM="back and save"; \
+char	txtLIPOVOLTAGELOW[] 	PROGMEM="LiPo Voltage low"; \
 char	txtLIIONVOLTAGE[] 	PROGMEM="LiIon Voltage"; \
 char	txtLIFEPOVOLT[] 	PROGMEM="LiFePo4 Volt."; \
 char	txtSTORAGEVOLTAGE[] 	PROGMEM="Storage Voltage"; \
-char	txtDISCHARGEVOLTAGE[] 	PROGMEM="Discharge Voltage"; \
+char	txtSTORAGEVOLTAGEHIGH[] 	PROGMEM="Storage Voltage high"; \
 char	txtREFRESHPERIOD[] 	PROGMEM="Refresh-period"; \
 
 #endif
@@ -115,19 +128,22 @@ char	txtREFRESHPERIOD[] 	PROGMEM="Refresh-period"; \
 #define MENUE_PARAM_VARDEF \
 myPar_t myPar = { \
 /*parCurrent*/ {	0, 100, 10000, -1, mA}, \
-/*parChVoltSET*/ {	0, 3000, 4200, 10, mV}, \
-/*parBalActVoltSET*/ {	0, 3000, 4200, 100, mV}, \
-/*parMaxcap*/ {	0, 0, 10000, 100, mAh}, \
-/*parMaxtime*/ {	0, 1, 6000, 1, min}, \
-/*parChVoltLiPo*/ {	0, 3000, 4200, 10, mV}, \
-/*parBalActVoltLiPo*/ {	0, 3000, 4200, 100, mV}, \
-/*parVoltLiIo*/ {	0, 3500, 4200, 10, mV}, \
-/*parBalActVoltLiIo*/ {	0, 3000, 4200, 100, mV}, \
-/*parVoltLiFe*/ {	0, 2000, 3700, 10, mV}, \
-/*parBalActVoltLiFe*/ {	0, 3000, 4200, 100, mV}, \
-/*parChtVoltStorage*/ {	0, 2000, 4100, 10, mV}, \
-/*parBalActVoltStor*/ {	0, 3000, 4200, 100, mV}, \
-/*parVoltDisch*/ {	0, 3000, 4200, 10, mV}, \
+/*parChVoltSET*/ {	0, 3300, 4250, 10, mV}, \
+/*parBalActVoltSET*/ {	0, 3200, 4300, 100, mV}, \
+/*parMaxcap*/ {	0, 250, 30000, 250, mAh}, \
+/*parMaxtime*/ {	0, 0, 9000, 20, min}, \
+/*parChVoltLiPo1*/ {	0, 3300, 4250, 10, mV}, \
+/*parBalActVoltLiPo1*/ {	0, 3200, 4300, 100, mV}, \
+/*parChVoltLiPo2*/ {	0, 3300, 4250, 10, mV}, \
+/*parBalActVoltLiPo2*/ {	0, 3200, 4300, 100, mV}, \
+/*parVoltLiIo*/ {	0, 3300, 4250, 10, mV}, \
+/*parBalActVoltLiIo*/ {	0, 3200, 4300, 100, mV}, \
+/*parVoltLiFe*/ {	0, 3300, 4250, 10, mV}, \
+/*parBalActVoltLiFe*/ {	0, 3200, 4300, 100, mV}, \
+/*parChtVoltStorage1*/ {	0, 3300, 4250, 10, mV}, \
+/*parBalActVoltStor1*/ {	0, 3200, 4300, 100, mV}, \
+/*parChtVoltStorage2*/ {	0, 3300, 4250, 10, mV}, \
+/*parBalActVoltStor2*/ {	0, 3200, 4300, 100, mV}, \
 /*parRefreshPeriod*/ {	0, 1, 10000, 10, ms}, \
 /*parMinSupplyVolt*/ {	0, 8000, 20000, 500, mV}, \
 };
@@ -139,51 +155,62 @@ myPar_t myPar = { \
 MenuItem_t m_items[MENUESIZE] = { \
 	/* 0*/	{txtREADY,	 0,	 0,	1,	0,	FLASH}, \
 	/* 1*/	{txtPRESETS,	 0,	 0,	9,	0,	FLASH}, \
-	/* 2*/	{txtCURRENTSETPOINTMA,	 0,	 &myPar.parCurrent,	0,	0,	FLASH}, \
-	/* 3*/	{txtCELLVOLTAGESETMV,	 0,	 &myPar.parChVoltSET,	0,	0,	FLASH}, \
-	/* 4*/	{txtBALACTIVVOLTAGEMV,	 0,	 &myPar.parBalActVoltSET,	0,	0,	FLASH}, \
-	/* 5*/	{txtMAXCAPACITYMAH,	 0,	 &myPar.parMaxcap,	0,	0,	FLASH}, \
-	/* 6*/	{txtMAXTIMEMIN,	 0,	 &myPar.parMaxtime,	0,	0,	FLASH}, \
-	/* 7*/	{txtSETUP,	 0,	 0,	15,	0,	FLASH}, \
+	/* 2*/	{txtCURRENTSETPOINT,	 0,	 &myPar.parCurrent,	0,	0,	FLASH}, \
+	/* 3*/	{txtCELLVOLTAGESET,	 0,	 &myPar.parChVoltSET,	0,	0,	FLASH}, \
+	/* 4*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltSET,	0,	0,	FLASH}, \
+	/* 5*/	{txtMAXCAPACITY,	 0,	 &myPar.parMaxcap,	0,	0,	FLASH}, \
+	/* 6*/	{txtMAXTIME,	 0,	 &myPar.parMaxtime,	0,	0,	FLASH}, \
+	/* 7*/	{txtSETUP,	 0,	 0,	16,	0,	FLASH}, \
 	/* 8*/	{txtSHOWLASTVALUES,	 ActionShowLast,	 0,	0,	0,	FLASH}, \
-	/* 9*/	{txtSTORAGE,	 ActionChargeModeStorage,	 0,	0,	1,	FLASH}, \
-	/* 10*/	{txtCHARGELIPO,	 ActionChargeModeLiPo,	 0,	0,	1,	FLASH}, \
-	/* 11*/	{txtCHARGELIIO,	 ActionChargeModeLiIo,	 0,	0,	1,	FLASH}, \
+	/* 9*/	{txtSTORAGE,	 ActionChargeModeStorage1,	 0,	0,	1,	FLASH}, \
+	/* 10*/	{txtCHARGELIPO,	 ActionChargeModeLiPo1,	 0,	0,	1,	FLASH}, \
+	/* 11*/	{txtSTORAGEHIGH,	 ActionChargeModeStorage2,	 0,	0,	1,	FLASH}, \
 	/* 12*/	{txtCHARGELIFE,	 ActionChargeModeLiFe,	 0,	0,	1,	FLASH}, \
-	/* 13*/	{txtDISCHARGE,	 ActionChargeModeDischarge,	 0,	0,	1,	FLASH}, \
-	/* 14*/	{txtBACK,	 0,	 0,	1,	1,	FLASH}, \
-	/* 15*/	{txtBACK,	 0,	 0,	7,	7,	FLASH}, \
-	/* 16*/	{txtPRESETSETUP,	 0,	 0,	21,	7,	FLASH}, \
-	/* 17*/	{txtPWM,	 0,	 0,	42,	7,	FLASH}, \
-	/* 18*/	{txtMINSUPPLY,	 0,	 &myPar.parMinSupplyVolt,	0,	7,	FLASH}, \
-	/* 19*/	{txtCALIBRATIONH,	 actCalH,	 0,	0,	7,	FLASH}, \
-	/* 20*/	{txtCALIBRATIONL,	 actCalL,	 0,	0,	7,	FLASH}, \
-	/* 21*/	{txtBACK,	 0,	 0,	16,	16,	FLASH}, \
-	/* 22*/	{txtLITHIUMPOLYMER,	 0,	 0,	28,	16,	FLASH}, \
-	/* 23*/	{txtLITHIUMION,	 0,	 0,	31,	16,	FLASH}, \
-	/* 24*/	{txtLIFEPO,	 0,	 0,	34,	16,	FLASH}, \
-	/* 25*/	{txtSTORAGE,	 0,	 0,	37,	16,	FLASH}, \
-	/* 26*/	{txtDISCHARGE,	 0,	 0,	40,	16,	FLASH}, \
-	/* 27*/	{txtBACKTOMAIN,	 0,	 0,	1,	16,	FLASH}, \
-	/* 28*/	{txtBACK,	 0,	 0,	22,	22,	FLASH}, \
-	/* 29*/	{txtLIPOVOLTAGE,	 0,	 &myPar.parChVoltLiPo,	0,	22,	FLASH}, \
-	/* 30*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltLiPo,	0,	22,	FLASH}, \
-	/* 31*/	{txtBACK,	 0,	 0,	23,	23,	FLASH}, \
-	/* 32*/	{txtLIIONVOLTAGE,	 0,	 &myPar.parVoltLiIo,	0,	23,	FLASH}, \
-	/* 33*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltLiIo,	0,	23,	FLASH}, \
+	/* 13*/	{txtCHARGELIIO,	 ActionChargeModeLiIo,	 0,	0,	1,	FLASH}, \
+	/* 14*/	{txtCHARGELIPOLOW,	 ActionChargeModeLiPo2,	 0,	0,	1,	FLASH}, \
+	/* 15*/	{txtBACK,	 0,	 0,	1,	1,	FLASH}, \
+	/* 16*/	{txtBACK,	 0,	 0,	7,	7,	FLASH}, \
+	/* 17*/	{txtPRESETSETUP,	 0,	 0,	22,	7,	FLASH}, \
+	/* 18*/	{txtPWM,	 0,	 0,	54,	7,	FLASH}, \
+	/* 19*/	{txtMINSUPPLY,	 0,	 &myPar.parMinSupplyVolt,	0,	7,	FLASH}, \
+	/* 20*/	{txtCALIBRATIONH,	 actCalH,	 0,	0,	7,	FLASH}, \
+	/* 21*/	{txtCALIBRATIONL,	 actCalL,	 0,	0,	7,	FLASH}, \
+	/* 22*/	{txtBACK,	 0,	 0,	17,	17,	FLASH}, \
+	/* 23*/	{txtLITHIUMPOLYMER,	 0,	 0,	30,	17,	FLASH}, \
+	/* 24*/	{txtLITHIUMPOLYMERLOW,	 0,	 0,	34,	17,	FLASH}, \
+	/* 25*/	{txtLITHIUMION,	 0,	 0,	38,	17,	FLASH}, \
+	/* 26*/	{txtLIFEPO,	 0,	 0,	42,	17,	FLASH}, \
+	/* 27*/	{txtSTORAGE,	 0,	 0,	46,	17,	FLASH}, \
+	/* 28*/	{txtSTORAGEHIGH,	 0,	 0,	50,	17,	FLASH}, \
+	/* 29*/	{txtBACKTOMAIN,	 0,	 0,	1,	17,	FLASH}, \
+	/* 30*/	{txtBACK,	 0,	 0,	23,	23,	FLASH}, \
+	/* 31*/	{txtLIPOVOLTAGE,	 0,	 &myPar.parChVoltLiPo1,	0,	23,	FLASH}, \
+	/* 32*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltLiPo1,	0,	23,	FLASH}, \
+	/* 33*/	{txtBACKANDSAVE,	 savePreset,	 0,	0,	23,	FLASH}, \
 	/* 34*/	{txtBACK,	 0,	 0,	24,	24,	FLASH}, \
-	/* 35*/	{txtLIFEPOVOLT,	 0,	 &myPar.parVoltLiFe,	0,	24,	FLASH}, \
-	/* 36*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltLiFe,	0,	24,	FLASH}, \
-	/* 37*/	{txtBACK,	 0,	 0,	25,	25,	FLASH}, \
-	/* 38*/	{txtSTORAGEVOLTAGE,	 0,	 &myPar.parChtVoltStorage,	0,	25,	FLASH}, \
-	/* 39*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltStor,	0,	25,	FLASH}, \
-	/* 40*/	{txtBACK,	 0,	 0,	26,	26,	FLASH}, \
-	/* 41*/	{txtDISCHARGEVOLTAGE,	 0,	 &myPar.parVoltDisch,	0,	26,	FLASH}, \
-	/* 42*/	{txtBACK,	 0,	 0,	17,	17,	FLASH}, \
-	/* 43*/	{txtREFRESHPERIOD,	 0,	 &myPar.parRefreshPeriod,	0,	17,	FLASH}, \
+	/* 35*/	{txtLIPOVOLTAGELOW,	 0,	 &myPar.parChVoltLiPo2,	0,	24,	FLASH}, \
+	/* 36*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltLiPo2,	0,	24,	FLASH}, \
+	/* 37*/	{txtBACKANDSAVE,	 savePreset,	 0,	0,	24,	FLASH}, \
+	/* 38*/	{txtBACK,	 0,	 0,	25,	25,	FLASH}, \
+	/* 39*/	{txtLIIONVOLTAGE,	 0,	 &myPar.parVoltLiIo,	0,	25,	FLASH}, \
+	/* 40*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltLiIo,	0,	25,	FLASH}, \
+	/* 41*/	{txtBACKANDSAVE,	 savePreset,	 0,	0,	25,	FLASH}, \
+	/* 42*/	{txtBACK,	 0,	 0,	26,	26,	FLASH}, \
+	/* 43*/	{txtLIFEPOVOLT,	 0,	 &myPar.parVoltLiFe,	0,	26,	FLASH}, \
+	/* 44*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltLiFe,	0,	26,	FLASH}, \
+	/* 45*/	{txtBACKANDSAVE,	 savePreset,	 0,	0,	26,	FLASH}, \
+	/* 46*/	{txtBACK,	 0,	 0,	27,	27,	FLASH}, \
+	/* 47*/	{txtSTORAGEVOLTAGE,	 0,	 &myPar.parChtVoltStorage1,	0,	27,	FLASH}, \
+	/* 48*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltStor1,	0,	27,	FLASH}, \
+	/* 49*/	{txtBACKANDSAVE,	 savePreset,	 0,	0,	27,	FLASH}, \
+	/* 50*/	{txtBACK,	 0,	 0,	28,	28,	FLASH}, \
+	/* 51*/	{txtSTORAGEVOLTAGEHIGH,	 0,	 &myPar.parChtVoltStorage2,	0,	28,	FLASH}, \
+	/* 52*/	{txtBALACTIVVOLTAGE,	 0,	 &myPar.parBalActVoltStor2,	0,	28,	FLASH}, \
+	/* 53*/	{txtBACKANDSAVE,	 savePreset,	 0,	0,	28,	FLASH}, \
+	/* 54*/	{txtBACK,	 0,	 0,	18,	18,	FLASH}, \
+	/* 55*/	{txtREFRESHPERIOD,	 0,	 &myPar.parRefreshPeriod,	0,	18,	FLASH}, \
 };
 #endif
 
 //******** END OF AUTO-GENERATED HEADER DO NOT EDIT!!! *********
-
 
